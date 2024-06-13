aa663f618f9403a1d6e1070d68e5bf0162966506
Your Name <you@example.com>
2024-06-13 08:55:20 +0000

    adding script functionality and linking everything back to the config

---
+++ configs/pseudo_labeling/bash_scripts/test.bash
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Define default values for the parameters
+CONFIG_FILE="configs/pseudo_labeling/config_files/test_2.yaml"
+USE_GPU=1
+OUTPUT_DIR="outputs/DEV_DELETE_THIS_MORE/TEST_2"
+
+# Print the parameters for verification
+echo "Using the following parameters:"
+echo "CONFIG_FILE: $CONFIG_FILE"
+echo "USING GPU: $USE_GPU"
+echo "OUTPUT DIR: $OUTPUT_DIR"
+
+
+# Run the Python training script with the specified arguments
+python pseudo_labeling_train_net.py --use_gpu $USE_GPU --config $CONFIG_FILE OUTPUT_DIR $OUTPUT_DIR 
